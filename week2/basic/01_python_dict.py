8d6c7309ab17dc19634fe36607bc4c9173afe86b
GitHubHyeonokKim <GitHubkyok9001@gmail.com>
2026-03-10 11:15:57 +0900

    complete week2 04 brute force

--- week2/basic/01_python_dict.py
+++ week2/basic/01_python_dict.py
@@ -42,19 +42,28 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
-    scores = [student["score"] for student in students]
+    # scores = [student["score"] for student in students]
+    score =[]
 
+    for i in students:
+        score.append(i["score"])
     
     # TODO: 평균 점수를 계산하세요
-    average = sum(scores)/len(scores)
+    average = sum(score)/len(score)
     
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
-    above_average_students = [
-        student["name"]
-        for student in students
-        if student["score"] >= average
-    ]
-    
+    # above_average_students = [
+    #     student["name"]
+    #     for student in students
+    #     if student["score"] >= average
+    # ]
+
+    above_average_students = []
+
+    for i in students:
+        if i["score"] >= average:
+            above_average_students.append(i["name"])
+
     return average, above_average_students
 
 
