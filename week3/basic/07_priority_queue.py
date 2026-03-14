7f62291b262930013c4cadd4032c1e236c0408a9
GitHubHyeonokKim <GitHubkyok9001@gmail.com>
2026-03-14 15:59:35 +0900

    complete week3 07 priority queue

--- week3/basic/07_priority_queue.py
+++ week3/basic/07_priority_queue.py
@@ -39,17 +39,21 @@ def process_emergency_room(patients):
     # TODO: 빈 힙 생성
     heap = []
     
-    
+
     # TODO: 모든 환자를 힙에 추가
-    pass
+    for patient in patients:
+        heapq.heappush(heap, (patient[1],patient[0]))
         
     processed = []
     
     # TODO: 힙이 비어있지 않은 동안 반복
     ## 힙에서 우선순위가 가장 높은 환자 꺼내기
     ## 환자 처리
-    pass
-        
+    while heap:
+        patient = heapq.heappop(heap)
+        print(f"처리: {patient[1]} (우선순위: {patient[0]})")
+        processed.append(patient[1])
+
     return processed
 
 # 테스트 케이스
