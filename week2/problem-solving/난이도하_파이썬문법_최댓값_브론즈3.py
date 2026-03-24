09a051cda169706cfd4bfd5bbf1b3e4d539a7cb5
HyeonokKim <hyok9001@gmail.com>
2026-03-25 00:13:03 +0900

    feat: implement maximum value calculation and position retrieval in Python

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,15 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+# numbers = [int(input()) for _ in range(9)]
+
+numbers = []
+for i in range(9):
+    numbers.append(int(input()))
+
+
+max_value = max(numbers)
+position = numbers.index(max_value) + 1
+
+print(max_value)
+print(position)
